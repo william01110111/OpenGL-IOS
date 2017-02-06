ac7cf7e84695b7c9616c35f9c8afc359dc855e3a
William W Wold <william01110111@gmail.com>
2017-02-06 12:26:49 -0800

    began to implement text loading

--- OpenGL test/GLToVideo.swift
+++ OpenGL test/GLToVideo.swift
@@ -40,14 +40,14 @@ class GLToVideo: WidapGLView {
 		+		"if (dstSq<0.9*0.9 && dstSq>0.7*0.7) { "
 		//+			"float cycle = 0.33; "
 		+			"float ang = degrees(atan(fragUV.y, fragUV.x))/360.0; "
-		+			"gl_FragColor.r=1.0-mod(ang-cycle*1.0+0.5, 1.0); "
-		+			"gl_FragColor.g=1.0-mod(ang-cycle*2.0, 1.0); "
-		+			"gl_FragColor.b=1.0-mod(ang-cycle*3.0, 1.0); "
+		+			"gl_FragColor.r=mod(ang-cycle*1.0+0.5, 1.0); "
+		+			"gl_FragColor.g=mod(ang-cycle*2.0, 1.0); "
+		+			"gl_FragColor.b=mod(ang-cycle*3.0, 1.0); "
 		+			"gl_FragColor.a = 1.0; "
 		+		"} else { "
 		+			"float maxDstSq = (sin(radians(cycle*360.0))+1.0)*0.2+0.1; "
 		+			"if (dstSq < maxDstSq * maxDstSq) {"
-		+				"gl_FragColor = vec4(0, 0, 0, 1); "
+		+				"gl_FragColor = vec4(0.0, 0.5, 1.0, 1); "
 		+			"} else { "
 		+				"gl_FragColor = vec4(0, 0, 0, 0); "
 		+			"} "
@@ -77,12 +77,99 @@ class GLToVideo: WidapGLView {
 		drawables.append(object)
 		
 		//object = WidapShape(verts: vertices, indices: [0, 1, 2], shader: ShaderProgram(vert: vertShaderSrc, frag: fragShaderSrc))
+		
+		setupCapture()
 	}
 	
 	override func update() {
 		
+		captureFrame()
+		
 		cycle.val += 0.01
 		
 		cycle.val = cycle.val - Float(Int(cycle.val))
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	var assetWriterPixelBufferInput: AVAssetWriterInputPixelBufferAdaptor?
+	
+	func setupCapture() {
+		
+		let width = 200
+		let height = 200
+		
+		let url = URL(fileURLWithPath: "widap_output_video.mp4")
+		
+		do {
+			let assetWriter = try AVAssetWriter(outputURL: url, fileType: AVFileTypeMPEG4)
+			
+			var outputSettings = [String: Any]();
+			
+			outputSettings[AVVideoCodecKey] = AVVideoCodecH264
+			outputSettings[AVVideoWidthKey] = width
+			outputSettings[AVVideoHeightKey] = height
+			
+			let assetWriterInput = AVAssetWriterInput(mediaType: AVMediaTypeVideo, outputSettings: outputSettings)
+			
+			assetWriterInput.expectsMediaDataInRealTime = true
+			
+			let sourcePixelBufferAttributesDictionary: [String: Any] = [
+				kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA,
+				kCVPixelBufferWidthKey as String: width,
+				kCVPixelBufferHeightKey as String: height
+			]
+			
+			assetWriterPixelBufferInput = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterInput, sourcePixelBufferAttributes: sourcePixelBufferAttributesDictionary)
+			
+			assetWriter.add(assetWriterInput)
+		} catch {
+			print("AVAssetWriter error")
+		}
+	}
+	
+	func captureFrame() {
+		/*
+		var pixel_buffer: CVPixelBuffer? = nil;
+		
+		CVPixelBufferPool
+		
+		CVPixelBufferPoolCreatePixelBuffer (NULL, [assetWriterPixelBufferInput pixelBufferPool], &pixel_buffer);
+		if ((pixel_buffer == NULL) || (status != kCVReturnSuccess))
+		{
+			return;
+		}
+		else
+		{
+			CVPixelBufferLockBaseAddress(pixel_buffer, 0);
+			GLubyte *pixelBufferData = (GLubyte *)CVPixelBufferGetBaseAddress(pixel_buffer);
+			glReadPixels(0, 0, videoSize.width, videoSize.height, GL_RGBA, GL_UNSIGNED_BYTE, pixelBufferData);
+		}
+		
+		// May need to add a check here, because if two consecutive times with the same value are added to the movie, it aborts recording
+		CMTime currentTime = CMTimeMakeWithSeconds([[NSDate date] timeIntervalSinceDate:startTime],120);
+		
+		if(![assetWriterPixelBufferInput appendPixelBuffer:pixel_buffer withPresentationTime:currentTime])
+		{
+			NSLog(@"Problem appending pixel buffer at time: %lld", currentTime.value);
+		}
+		else
+		{
+			//        NSLog(@"Recorded pixel buffer at time: %lld", currentTime.value);
+		}
+		CVPixelBufferUnlockBaseAddress(pixel_buffer, 0);
+		
+		CVPixelBufferRelease(pixel_buffer);
+		*/
 	}
 }
---
+++ OpenGL test/TextRender.swift
@@ -0,0 +1,124 @@
+//
+//  SpinnerView.swift
+//  OpenGL test
+//
+//  Created by William Wold on 2/2/17.
+//  Copyright © 2017 William Wold. All rights reserved.
+//
+
+import UIKit
+import GLKit
+
+class TextRender: WidapGLView {
+	
+	let vertShaderSrc = ""
+		+	"attribute vec4 pos; "
+		+	"attribute vec2 uv; "
+		+	"varying lowp vec2 fragUV; "
+		+	"void main(void) { "
+		+		"gl_Position = pos; "
+		//+		"fragColor = vec4(0.5, 0.0, 1.0, 1.0); "
+		+		"fragUV = uv; "
+		+	"} "
+	
+	/*
+	let fragShaderSrc = ""
+	+	"varying lowp vec4 fragColor; "
+	+	"varying lowp vec2 fragUV; "
+	+	"void main(void) { "
+	+		"gl_FragColor = fragColor; "
+	+	"} "
+	*/
+	
+	let spinnerFragShaderSrc = ""
+		+	"uniform lowp float cycle; "
+		+	"varying lowp vec2 fragUV; "
+		+	"precision lowp float; "
+		+	"void main(void) { "
+		+		"float dstSq = fragUV.x*fragUV.x+fragUV.y*fragUV.y; "
+		+		"if (dstSq<0.9*0.9 && dstSq>0.7*0.7) { "
+		//+			"float cycle = 0.33; "
+		+			"float ang = degrees(atan(fragUV.y, fragUV.x))/360.0; "
+		+			"gl_FragColor.r=1.0-mod(ang-cycle*1.0+0.5, 1.0); "
+		+			"gl_FragColor.g=1.0-mod(ang-cycle*2.0, 1.0); "
+		+			"gl_FragColor.b=1.0-mod(ang-cycle*3.0, 1.0); "
+		+			"gl_FragColor.a = 1.0; "
+		+		"} "
+		+		"else { "
+		+			"gl_FragColor = vec4(0, 0, 0, 0); "
+		+		"} "
+		+	"}"
+	
+	let vertices : [Vertex] = [
+		Vertex( 0.0,  0.25, 0.0),    // TOP
+		Vertex(-0.5, -0.25, 0.0),    // LEFT
+		Vertex( 0.5, -0.25, 0.0),    // RIGHT
+	]
+	
+	fileprivate var object = WidapShape()
+	
+	var cycle = UniformFloat(0.0)
+	
+	override func setup() {
+		
+		super.setup()
+		
+		let shader = ShaderProgram(vertAttribs: VertexAttributes, vert: vertShaderSrc, frag: spinnerFragShaderSrc)
+		
+		shader.addUniform(uniform: cycle, name: "cycle")
+		
+		object = FullRect(shader: shader)
+		
+		drawables.append(object)
+		
+		//object = WidapShape(verts: vertices, indices: [0, 1, 2], shader: ShaderProgram(vert: vertShaderSrc, frag: fragShaderSrc))
+		
+		//UIView *view = ... something ...;
+		
+		let view = UITextView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+		
+		view.text = "hello, this is some text in a view"
+		
+		// make space for an RGBA image of the view
+		//GLubyte *pixelBuffer = (GLubyte *)malloc(4 * view.bounds.size.width * view.bounds.size.height);
+		
+		let width: Int = Int(view.bounds.size.width)
+		let height: Int = Int(view.bounds.size.height)
+		
+		let pixelBuffer = malloc(4 * width * height)
+		
+		let colorSpace = CGColorSpaceCreateDeviceRGB()
+		let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+		guard let context = CGContext.init(data: pixelBuffer, width: width, height: height, bitsPerComponent: 8, bytesPerRow: 4*width, space: colorSpace, bitmapInfo: UInt32(bitmapInfo.rawValue)) else {
+			// cannot create context - handle error
+			print("context error or something")
+			return
+		}
+		
+		// create a suitable CoreGraphics context
+		//let context = CGBitmapContextCreate(pixelBuffer, view.bounds.size.width, view.bounds.size.height, 8, 4*view.bounds.size.width, colourSpace, CGBitmapInfo.alphaInfoMask, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+		
+		//CGColorSpaceRelease(colourSpace);
+		
+		// draw the view to the buffer
+		//[view.layer renderInContext:context];
+		
+		view.layer.render(in: context)
+		
+		var tex: GLuint = 0
+		
+		//glGenTextures(1, &tex)
+		
+		// upload to OpenGL
+		glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GL_RGBA, GLsizei(view.bounds.size.width), GLsizei(view.bounds.size.height), 0, GLenum(GL_RGBA), GLenum(GL_UNSIGNED_BYTE), pixelBuffer);
+		
+		free(pixelBuffer);
+	}
+	
+	override func update() {
+		
+		cycle.val += 0.01
+		
+		cycle.val = cycle.val - floor(cycle.val)
+	}
+}
